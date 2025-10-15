de4365b25d606896d6e4f902a18a44ef10ee7c66
black-adm <mattheuscontato17@gmail.com>
2025-10-15 09:15:57 -0300

    fix: draw player in screen

---
+++ game/player.go
@@ -0,0 +1,28 @@
+package game
+
+import (
+	"spacegame/assets"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Player struct {
+	image *ebiten.Image
+}
+
+func NewPlayer() *Player {
+	image := assets.PlayerSprite
+	return &Player{
+		image: image,
+	}
+}
+
+func (p *Player) Update() {
+
+}
+
+func (p *Player) Draw(screen *ebiten.Image) {
+  op := &ebiten.DrawImageOptions{}
+  op.GeoM.Translate(100, 100)
+  screen.DrawImage(p.image, op)
+}
